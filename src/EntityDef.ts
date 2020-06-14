d7af5214e7e861cd1e0d9fc12d515466cda14287
TiAiuto (Dynabook) <ti.aiuto@outlook.jp>
2020-06-14 16:39:12 +0900

    :heavy_plus_sign: EntityDefの定義も追加

--- src/EntityDef.ts
+++ src/EntityDef.ts
@@ -1,12 +1,21 @@
+import { EntityFieldDef } from "./EntityFieldDef";
+import { EntityRelationDef } from "./EntityRelationDef";
+
 export interface EntityDefArgs {
   name: string;
+  fields: EntityFieldDef[];
+  relations?: EntityRelationDef[] | undefined;
 }
 
-export class EntityDef implements EntityDefArgs {
+export class EntityDef implements Required<EntityDefArgs> {
   name: string;
+  fields: EntityFieldDef[];
+  relations: EntityRelationDef[];
 
   constructor(args: EntityDefArgs) {
     // TODO: ここに定義のバリデーション入れる
     this.name = args.name;
+    this.fields = args.fields;
+    this.relations = args.relations ?? [];
   }
 }
