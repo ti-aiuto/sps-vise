4004b3838a0f77546d6ef8269c67765f1eaad72f
TiAiuto (Dynabook) <ti.aiuto@outlook.jp>
2020-06-14 16:48:56 +0900

    :pencil2: export実装

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { EntityDef } from "./EntityDef";
+import { EntityFieldDef } from "./EntityFieldDef";
+import { EntityRelationDef } from "./EntityRelationDef";
+
+export { EntityDef, EntityFieldDef, EntityRelationDef };
