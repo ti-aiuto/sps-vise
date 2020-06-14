03326b173fd663e0567eb3c9e181438a6db2bfb3
TiAiuto (Dynabook) <ti.aiuto@outlook.jp>
2020-06-14 16:54:19 +0900

    :heavy_plus_sign: EntityDefにコメント定義できるようにした

--- src/EntityDef.ts
+++ src/EntityDef.ts
@@ -5,17 +5,20 @@ export interface EntityDefArgs {
   name: string;
   fields: EntityFieldDef[];
   relations?: EntityRelationDef[] | undefined;
+  comment?: string | undefined;
 }
 
 export class EntityDef implements Required<EntityDefArgs> {
   name: string;
   fields: EntityFieldDef[];
   relations: EntityRelationDef[];
+  comment: string;
 
   constructor(args: EntityDefArgs) {
     // TODO: ここに定義のバリデーション入れる
     this.name = args.name;
     this.fields = args.fields;
     this.relations = args.relations ?? [];
+    this.comment = args.comment ?? '';
   }
 }
