e1ef160b16651151355d6e6bedc5e9cc957ed1d4
TiAiuto (Dynabook) <ti.aiuto@outlook.jp>
2020-06-14 18:18:31 +0900

    :pencil2: 修正

--- src/SheetBuilder.ts
+++ src/SheetBuilder.ts
@@ -38,10 +38,14 @@ export class SheetBuilder implements Required<SheetBuilderArgs>{
     this.entityDef.fields.forEach((field, index) => {
       this.sheet.setValue(this.baseY + 0, this.baseX + index, field.comment);
       this.sheet.setValue(this.baseY + 1, this.baseX + index, field.name);
+      const rowFrom = this.baseY + 2;
+      const columnFrom = this.baseX + index;
+      const rowTo = this.baseY + 2 + MAX_SIZE - 1;
+      const columnTo = this.baseX + index;
       if (field.dataType === 'integer') {
-        this.sheet.setRegexConditionalFormat(this.baseY + 2, this.baseY + 2 + MAX_SIZE - 1, this.baseX + index, this.baseX + index, "[+-]?\\d+", "#00F");
+        this.sheet.setRegexConditionalFormat(rowFrom, columnFrom, rowTo, columnTo, "[+-]?\\d+", "#0000FF");
       } else if (field.dataType === 'number') {
-        this.sheet.setRegexConditionalFormat(this.baseY + 2, this.baseY + 2 + MAX_SIZE - 1, this.baseX + index, this.baseX + index, " [+-]?(?:\\d+\\.?\\d*|\\.\\d+)", "#00F");
+        this.sheet.setRegexConditionalFormat(rowFrom, columnFrom, rowTo, columnTo, " [+-]?(?:\\d+\\.?\\d*|\\.\\d+)", "#0000FF");
       }
     });
   }
