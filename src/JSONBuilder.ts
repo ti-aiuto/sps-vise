0c35e4b6b9b491d310e3ca417df98181eded81ac
TiAiuto (Dynabook) <ti.aiuto@outlook.jp>
2020-06-14 23:17:23 +0900

    :heavy_plus_sign: relation部分の実装

--- src/JSONBuilder.ts
+++ src/JSONBuilder.ts
@@ -36,33 +36,52 @@ export class JSONBuilder implements JSONBuilderArgs {
 
     const result: EntityValue[] = [];
     const entityCellValues = sheet.getValuesRange(
-      entitySheetSetting.baseRow,
+      entitySheetSetting.baseRow + 3,
       entitySheetSetting.baseColumn,
       entitySheetSetting.size + 3,
       entitySheetSetting.entityDef.fields.length
     );
 
     entityCellValues.forEach((row) => {
-      if (row.every((item) => item.length > 0)) {
+      if (row.some((item) => item.length > 0)) {
         result.push(this.buildObject(entitySheetSetting.entityDef, row));
       }
     });
 
-    // // Entityのリレーションの定義
-    // entitySheetSetting.entityDef.relations.forEach((relation, index) => {
-    //   const currentColumnPosition = entitySheetSetting.entityDef.fields.length + 1 + index * 5;
+    // Entityのリレーションの定義
+    entitySheetSetting.entityDef.relations.forEach((relation, index) => {
+      result.forEach((item) => item[relation.name] = []);
 
-    //   sheet.getValuesRange(
-    //     entitySheetSetting.baseRow,
-    //     entitySheetSetting.baseColumn + currentColumnPosition,
-    //     entitySheetSetting.size + 3,
-    //     4
-    //   );
-    // });
+      const currentColumnPosition = entitySheetSetting.entityDef.fields.length + 1 + index * 5;
+      const relationCellValues = sheet.getValuesRange(
+        entitySheetSetting.baseRow + 3,
+        entitySheetSetting.baseColumn + currentColumnPosition,
+        entitySheetSetting.size + 3,
+        4 // TODO: この辺の値どこかに定数化する
+      );
+
+      relationCellValues.forEach((row, index) => {
+        if (row.some((item) => item.length > 0)) {
+          const homeId = row[0];
+          const foreignId = row[2];
+          if (homeId.length === 0 || foreignId.length === 0) {
+            throw new Error(`${index + 1}番目の項目のIDが未入力`);
+          }
+          // TODO: ここの値の比較も切り出したい
+          const entity = result.find((item) => item['id'] + '' === homeId + '');
+          if (!entity) {
+            throw new Error(`${index + 1}番目の項目のIDが不明`);
+          }
+          // NOTICE: idはstring固定仕様にしたい
+          (entity[relation.name] as unknown as string[]).push(foreignId);
+        }
+      });
+    });
 
     return result;
   }
 
+  // TODO: ここの処理を切り出す
   private buildObject(entityDef: EntityDef, row: string[]): EntityValue {
     const result: EntityValue = {};
     entityDef.fields.forEach((field, index) => {
