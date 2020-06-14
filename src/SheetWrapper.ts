4be67e447f7804453d96fec704c0cc52ce02fc10
TiAiuto (Dynabook) <ti.aiuto@outlook.jp>
2020-06-14 18:05:36 +0900

    :heavy_plus_sign: 条件付き書式の初期化メソッド実装

--- src/SheetWrapper.ts
+++ src/SheetWrapper.ts
@@ -1,6 +1,7 @@
 export interface SheetWrapper {
   setValue(row: number, column: number, value: string): void;
-  setRegexCondition(rowFrom: number, columnFrom: number, rowTo: number, columnTo: number, regex: string, backgroundColor: string): void;
+  setRegexConditionalFormat(rowFrom: number, columnFrom: number, rowTo: number, columnTo: number, regex: string, backgroundColor: string): void;
+  clearConditionalFormat(): void;
 }
 
 export class GoogleSheetWrpaper implements SheetWrapper {
@@ -13,7 +14,11 @@ export class GoogleSheetWrpaper implements SheetWrapper {
     this.googleSheet.getRange(row, column).setValue(value);
   }
 
-  setRegexCondition(rowFrom: number, columnFrom: number, rowTo: number, columnTo: number, regex: string, backgroundColor: string): void {
+  clearConditionalFormat(): void {
+    this.googleSheet.setConditionalFormatRules([]);
+  }
+
+  setRegexConditionalFormat(rowFrom: number, columnFrom: number, rowTo: number, columnTo: number, regex: string, backgroundColor: string): void {
     const rules = this.googleSheet.getConditionalFormatRules();
     for (let row = rowFrom; row <= rowTo; row++) {
       for (let column = columnFrom; column <= columnTo; column++) {
