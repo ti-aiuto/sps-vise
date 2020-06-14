202a1e03127225085b32cae3ad5897ad04379763
TiAiuto (Dynabook) <ti.aiuto@outlook.jp>
2020-06-14 21:14:07 +0900

    :pencil2: 要らない定義けした

--- src/EntityRelationDef.ts
+++ src/EntityRelationDef.ts
@@ -1,24 +1,16 @@
 export interface EntityRelationDefArgs {
   relationType: 'one_to_many' | 'many_to_many';
-  fieldName?: string | undefined;
   targetEntityName: string;
-  targetFieldName: string;
 }
 
 export class EntityRelationDef implements EntityRelationDefArgs {
   relationType: 'one_to_many' | 'many_to_many';
 
-  fieldName: string;
-
   targetEntityName: string;
-
-  targetFieldName: string;
 
   constructor(args: EntityRelationDefArgs) {
     // TODO: ここに定義のバリデーション入れる
     this.relationType = args.relationType;
-    this.fieldName = args.fieldName ?? 'id';
     this.targetEntityName = args.targetEntityName;
-    this.targetFieldName = args.targetFieldName;
   }
 }
